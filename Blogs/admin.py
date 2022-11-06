20868318eff561eb16e3a36dd2843158b2d81ccc
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2022-11-06 10:40:25 +0530

    Added functionality of multiple user login to admin panel and permission to edit blogs posted by the logged in user

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Blog)
+# admin.site.register(Blog)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser:
+            self.exclude = ("author", )
+        form = super(BlogAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
