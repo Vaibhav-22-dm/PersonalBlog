98d7f6e68d1cde603547ac3ff7f6c9bed4063d29
Vaibhav-22-dm <vaibhavdmohite22@gmail.com>
2023-03-01 22:14:52 +0530

    Removed whitenoise

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -46,7 +46,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,8 +145,6 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
