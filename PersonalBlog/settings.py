4c5c642d31a62f9f3f32c6fce3c7a8890a3d2063
Vaibhav-22-dm <vaibhavdmohite22@gmail.com>
2023-03-07 22:57:20 +0530

    Added postgres db

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -96,11 +96,11 @@ DATABASES={
 #       'HOST':'localhost',
 #       'PORT':'5432',
 #    },
-   'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgres_db':{
+#    'default':{
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
       'NAME': os.environ.get('DB_NAME'),
       'USER':os.environ.get('DB_USER'),
